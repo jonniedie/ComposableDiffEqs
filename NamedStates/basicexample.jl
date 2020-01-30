a32a728abb64ca5ecbb3ce7a210de1a99146ed3a
Jonnie <jonniedie@yahoo.com>
2020-01-29 22:43:15 -0500

    Made example better.

--- NamedStates/basicexample.jl
+++ NamedStates/basicexample.jl
@@ -1,27 +1,54 @@
+"""
+Example taken from:
+https://github.com/JuliaDiffEq/ModelingToolkit.jl/issues/36#issuecomment-536221300
+"""
+
 include("NamedViewVector.jl")
 
 using DifferentialEquations
+using Plots
+using Interact
 
-function de1(dx, x, p, t)
-    dx = x
+theme(:juno)
+settheme!(:nativehtml)
+
+
+function lorenz!(du, u, (p, f), t)
+    x, y, z = u.x, u.y, u.z
+    σ, ρ, β = p.σ, p.ρ, p.β
+
+    du.x = σ*(y - x)
+    du.y = x*(ρ - z) - y + f
+    du.z = x*y - β*z
     return nothing
 end
 
-function de2(dx, x, p, t)
-    dx.x1 = x.x2
-    dx.x2 = -p*x.x1
+function lotka!(du, u, (p, f), t)
+    x, y = u.x, u.y
+    α, β, γ, δ = p.α, p.β, p.γ, p.δ
+
+    du.x = α*x - β*x*y + f
+    du.y = -γ*y + δ*x*y
     return nothing
 end
 
-function de(dx, x, p, t)
-    de1(dx.y, x.y, x.x.x1, t)
-    de2(dx.x, x.x, x.y, t)
+function composed!(du, u, p, t)
+    lorenz!(du.lorenz, u.lorenz, (p, u.lotka.x), t)
+    lotka!(du.lotka, u.lotka, (p, u.lorenz.x), t)
     return nothing
 end
 
-p = 0.0
+p = (α=1.0, β=2.5, γ=3.1, δ=0.5, ρ=1.0, σ=0.1)
 
-x0 = NamedViewVector{Float64}((x=(x1=1.0, x2=2.0), y=3.0))
+prange = 0 : 0.01 : 10
 
-prob = ODEProblem(de, x0, (0.0, 10.0), p)
+tspan = (0.0, 20.0)
+lorenz_ic = (x=0.0, y=0.0, z=0.0)
+lotka_ic = (x=1.0, y=1.0)
+
+u₀ = NamedViewVector{Float64}((lorenz=lorenz_ic, lotka=lotka_ic))
+
+prob = ODEProblem(composed!, u₀, tspan, p)
 sol = solve(prob, Tsit5())
+
+plot(sol)
