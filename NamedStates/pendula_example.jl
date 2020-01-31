319a09fb40ece17fbd175a1a96d8ffe473ecc88b
Jonnie <jonniedie@yahoo.com>
2020-01-30 23:36:26 -0500

    Some more exampling

--- NamedStates/pendula_example.jl
+++ NamedStates/pendula_example.jl
@@ -14,19 +14,25 @@ const G0 = 9.80665
 
 # -- Component equations -------------------------------------------------------------------
 function pendulum!(dθ, θ, p, t)
+    m, L, ΣM = p.mass, p.L, p.ΣM
+
     dθ.pos = θ.vel
-    dθ.vel = p.M - G0/p.L*sin(θ.pos)
+    dθ.vel = (ΣM - m*G0*sin(θ.pos)) / (m*L)
     return nothing
 end
 
-function cart!(dx, x, F, t)
+function cart!(dx, x, p, t)
+    m, ΣF = p.mass, p.ΣF
+
     dx.pos = x.vel
-    dx.vel = F
+    dx.vel = ΣF/m
     return nothing
 end
 
 function composed!(du, u, p, t)
+    m, L = p.pend.mass, p.pend.L
+    M = p.cart.mass
 
-    pendulum!(du.θ, u.θ, , t)
-    cart!(du.x, u.x, F, t)
+    pendulum!(du.θ, u.θ, (mass=m, L=L, ΣM=), t)
+    cart!(du.x, u.x, (mass=m+M, ΣF=), t)
 end
